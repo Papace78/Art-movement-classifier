628efe1f43d6603d9ab019108738b82900a9cb1e
Papace78 <140399494+Papace78@users.noreply.github.com>
2024-02-26 10:07:05 +0100

    Revert "Vgg2 dropout"

--- smn/model.py
+++ smn/model.py
@@ -1,45 +1,30 @@
-import sys
-
-from colorama import Fore, Style
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
-import matplotlib.pyplot as plt
 
+from colorama import Fore, Style
+import time
 
 def initialize_model():
     model = Sequential()
 
     model.add(Conv2D(32, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform', padding = 'same', input_shape = (512,512,3)))
-    model.add(BatchNormalization(momentum = 0.9))
     model.add(Conv2D(32, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform', padding = 'same'))
-    model.add(BatchNormalization(momentum = 0.9))
     model.add(MaxPooling2D())
-    model.add(Dropout(0.1))
 
     model.add(Conv2D(64, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform', padding = 'same'))
-    model.add(BatchNormalization(momentum = 0.9))
     model.add(Conv2D(64, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform', padding = 'same'))
-    model.add(BatchNormalization(momentum = 0.9))
     model.add(MaxPooling2D())
-    model.add(Dropout(0.4))
-
-    """model.add(Conv2D(128, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform'))
-    model.add(BatchNormalization(momentum = 0.9))
+    """
     model.add(Conv2D(128, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform'))
-    model.add(BatchNormalization(momentum = 0.9))
-    model.add(MaxPooling2D())
-    model.add(Dropout(0.4))"""
+    model.add(Conv2D(128, (3,3), 1, activation = 'relu', kernel_initializer='he_uniform'))
+    model.add(MaxPooling2D())"""
 
     model.add(Flatten())
 
     model.add(Dense(128, activation = 'relu'))
-    model.add(BatchNormalization(momentum = 0.9))
-    model.add(Dropout(0.5))
     model.add(Dense(13, activation = 'softmax'))
-
-    print("✅ Model initialized")
 
     return model
 
@@ -53,8 +38,6 @@ def compile_model(model,
         optimizer = Adam(learning_rate = learning_rate),
         loss = loss,
         metrics = metrics)
-
-    print("✅ Model compiled")
 
     return model
 
@@ -72,22 +55,6 @@ def evaluate_model(model, test_generator):
     loss = metrics["loss"]
     accuracy = metrics['accuracy']
 
-    print(f"✅ Model evaluated, accuracy: {round(accuracy*100, 2)}%")
+    print(f"✅ Model evaluated, MAE: {round(accuracy, 2)}")
 
     return metrics
-
-def learning_curves(history):
-    # plot loss
-    plt.subplot(211)
-    plt.title('Loss')
-    plt.plot(history.history['loss'], color='blue', label='train')
-    plt.plot(history.history['val_loss'], color='orange', label='test')
-    # plot accuracy
-    plt.subplot(212)
-    plt.title('Accuracy')
-    plt.plot(history.history['accuracy'], color='blue', label='train')
-    plt.plot(history.history['val_accuracy'], color='orange', label='test')
-    # save plot to file
-    filename = sys.argv[0].split('/')[-1]
-    plt.savefig(filename + '_plot.png')
-    plt.close()
