43143c2209e82deb0fec58368894f3eb7a24dcdc
Pascal Ghaname <pascaljunior49@gmail.com>
2024-03-07 16:32:52 +0100

    cli

--- transfer_learning/components/core.py
+++ transfer_learning/components/core.py
@@ -15,15 +15,29 @@ def classification(
     finetune=17, batch_size=32, image_size=(224, 224), validation_split=0.2, n_classes=8
 ):
     print('\nFinetune =', finetune)
+
+    #CREATE TRAINVAL and TEST DIRECTORIES
     if not "trainval_directory" in os.listdir("raw_data/wikiart/"):
         sort_files()
 
+    #FETCH THE DATA AND LOAD THEM IN A DATASET
     train, val, test = get_data(
         batch_size=batch_size, image_size=image_size, validation_split=validation_split
     )
 
+
+    #CREATE MODEL:
+    #   data augmentation
+    #   preprocess_input
+    #   vgg16 (cf finetune for trainable layers)
+    #   two dense layer
+    #   dropout 0.2
+    #   classification layer
+
     vgg16 = create_model(n_classes=n_classes, fine_tune=finetune)
 
+
+    #TRAIN MODEL
     print(Fore.BLUE + f"\nTraining model on {len(train)} rows..." + Style.RESET_ALL)
     history = train_model(vgg16, train, val)
 
@@ -31,15 +45,19 @@ def classification(
         f"✅ Model trained on {len(train)} rows with min val accuracy: {round(np.min(history.history['val_accuracy']), 2)}"
     )
 
+    #FINETUNE MODEL
     if finetune:
         print(Fore.BLUE + f"\nFinetuning model on {len(train)} rows..." + Style.RESET_ALL)
         history_fine = finetune_model(vgg16,history = history, train_dataset= train, validation_dataset=val)
 
 
+    #EVALUATE MODEL
     print(Fore.BLUE + f"\nTesting model on {len(test)} rows..." + Style.RESET_ALL)
     accuracy = evaluate_model(vgg16, test)
     print(f"✅ Model tested, accuracy: {round(accuracy*100, 2)}%")
 
+
+    #PLOT LEARNING CURVES
     if finetune:
         learning_curves(history_fine, title="finetunening")
     else:
