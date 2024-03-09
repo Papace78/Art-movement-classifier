0f1374b14d63b9b88b030258b3fd190aacfc43ec
Pascal Ghaname <pascaljunior49@gmail.com>
2024-03-09 19:01:26 +0100

    wip

--- app_folder/app_file.py
+++ app_folder/app_file.py
@@ -12,33 +12,33 @@ import matplotlib.pyplot as plt
 app = FastAPI()
 model = load_model(f"./model/tobedeleted.keras")
 
+
 # define root
 @app.get("/")
 def status():
     return {"API": "connected"}
 
 
-#@app.post("/file")
-#async def create_file(file: Annotated[bytes, File()]):
- #   return {"file_size": len(file)}
+# @app.post("/file")
+# async def create_file(file: Annotated[bytes, File()]):
+#   return {"file_size": len(file)}
 
 
 @app.post("/predict")
-async def create_upload_file(file: UploadFile= File(...)):
+async def create_upload_file(file: UploadFile = File(...)):
 
-    file.filename = f'{uuid.uuid4()}.jpg'
+    file.filename = f"{uuid.uuid4()}.jpg"
     contents = await file.read()
 
     with open(f"raw_data/{file.filename}", "wb") as f:
         f.write(contents)
 
-    img_path = os.path.join('raw_data',file.filename)
+    img_path = os.path.join("raw_data", file.filename)
     img = plt.imread(img_path)
 
-
     tensor_image = tf.convert_to_tensor(img)
-    tensor_image = tf.image.resize(img,(224,224), method = 'nearest')
-    tensor_image = tf.expand_dims(tensor_image, axis = 0)
+    tensor_image = tf.image.resize(img, (224, 224), method="nearest")
+    tensor_image = tf.expand_dims(tensor_image, axis=0)
 
     y_pred = model.predict(tensor_image)
 
@@ -46,5 +46,4 @@ async def create_upload_file(file: UploadFile= File(...)):
 
     return my_pred
 
-
-    #return FileResponse(img_path)
+    # return FileResponse(img_path)
